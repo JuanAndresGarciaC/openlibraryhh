7c5648971912eebf0ffc2348921719adf728ee64
Juan Andres Garcia C <JAGC@iMac-de-Juan.local>
2016-11-19 12:23:15 -0500

    Initial Commit

---
+++ LIbrosLibros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LIbrosLibros
+//
+//  Created by Juan Andres Garcia C on 19/11/16.
+//  Copyright © 2016 Juan Andres Garcia C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
