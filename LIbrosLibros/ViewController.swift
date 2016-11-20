cb84e2620c1f5dcfc099be7f185c9f3d111cd6b5
Juan Andres Garcia C <JAGC@iMac-de-Juan.local>
2016-11-20 12:06:51 -0500

    jh0lka

--- LIbrosLibros/ViewController.swift
+++ LIbrosLibros/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ISBN: UITextField!
+    
+    @IBAction func Clear(_ sender: Any) {
+        ISBN.text = ""
+    }
+    
+    @IBOutlet weak var Resultado: UITextView!
+    
+    @IBAction func Buscar(_ sender: Any) {
+    sincrono()
+    }
+    func sincrono() {
+        var urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN"+ISBN.text!
+        print(urls)
+        print(urls)
+        Resultado.text = urls
+        if urls == "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"{
+            
+            Resultado.text = "Porfavor escriba el ISBN"
+        }
+        
+            let url = NSURL(string: urls)
+            let datos:NSData? = NSData(contentsOf: url! as URL)
+            let texto = NSString(data:datos! as Data, encoding:String.Encoding.utf8.rawValue)
+            print(texto)
+            Resultado.text = texto as String!
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +47,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
